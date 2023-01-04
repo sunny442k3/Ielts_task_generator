1c8215136edf28cf9a7346caf2597da3aae422ae
sunny442k3 <phucvan.ai.442k@gmail.com>
2023-01-04 22:10:44 +0700

    Add data

--- process.py
+++ process.py
@@ -1,15 +1,19 @@
 import json 
 import requests
 from bs4 import BeautifulSoup
+from PyPDF2 import PdfReader, PdfFileWriter
 
 def load_json(filename):
     return json.load(open(filename, "r"))
 
 
-def get_content_page(url):
+def get_content_page(url, decode=True):
     r = requests.get(url)
     if r.status_code == 200:
-        return r.content.decode("utf8")
+        if decode:
+            return r.content.decode("utf8")
+        else:
+            return r.content
     return ""
 
 
@@ -52,5 +56,50 @@ def run_crawl():
     with open("./full_data.json", "w", encoding="utf-8") as f:
         json.dump(new_data, f)
 
+def crawl_from_list_url():
+    urls = load_json("./all_link.json")
+    data = []
+    for url in urls:
+        tmp_data = {}
+        tmp_data["url"] = url 
+        content = get_content_page(url, decode=False)
+        soup = BeautifulSoup(content, "html.parser")
+        art = soup.find_all("article")[0]
+        p = art.find_all("p", {"style": "font-size: 13px; font-family: Verdana"})[:-1]
+        text = []
+        for i in p:
+            tmp = i.get_text(separator="<br>").strip().replace("<br>", "\n")
+            tmp = tmp.split("\n")
+            text += tmp
+        tmp_data["paragraphs"] = text 
+        data.append(tmp_data)
+        print(url)
+    with open("./data/add_data_1.json", "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def pdf2txt():
+
+    file_path = './100_sample.pdf'
+    pdf = PdfReader(file_path)
+    print(pdf.pages[1])
+    with open('./100_sample.txt', 'w') as f:
+        for page_num in range(len(pdf.pages)):
+            # print('Page: {0}'.format(page_num))
+            pageObj = pdf.pages[page_num]
+
+            try: 
+                txt = pageObj.extractText()
+                print(''.center(100, '-'))
+            except:
+                pass
+            else:
+                f.write('Page {0}\n'.format(page_num+1))
+                f.write(''.center(100, '-'))
+                f.write(txt)
+        f.close()
+
 if __name__ == "__main__":
-    run_crawl()
+    # run_crawl()
+    # crawl_from_list_url()
+    pdf2txt()
